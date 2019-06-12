80da99bfd03463a0b8681bcc83f6f3cc4a9fc849
starikcetin <cetinsamedtarik@gmail.com>
2019-06-13 00:30:53 +0300

    grid generation is working, but it needs to be centered.

---
+++ hexfall-clone/Assets/game/code/HexagonGridBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace starikcetin.hexfallClone
+{
+    public class HexagonGridBuilder : MonoBehaviour
+    {
+        [SerializeField] private int _columnCount, _rowCount;
+        [SerializeField] private float _size;
+
+        private void Start()
+        {
+            BuildHexagonGrid(_size);
+        }
+
+        private void BuildHexagonGrid(float size)
+        {
+            for (var col = 0; col < _columnCount; col++)
+            {
+                for (var row = 0; row < _rowCount; row++)
+                {
+                    var offsetCoordinates = new OffsetCoordinates(col, row);
+
+                    var newHexagon = Instantiate(PrefabDatabase.Instance.Hexagon, transform);
+                    newHexagon.transform.position = offsetCoordinates.ToUnity(size);
+                }
+            }
+        }
+    }
+}
---
+++ hexfall-clone/Assets/game/code/PrefabDatabase.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PrefabDatabase : Singleton<PrefabDatabase>
+{
+    [SerializeField] private GameObject _hexagon;
+    public GameObject Hexagon => _hexagon;
+}
