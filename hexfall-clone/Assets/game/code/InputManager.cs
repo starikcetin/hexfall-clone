a897a0b4bab7ecbb7195e9edc743adce8714538f
starikcetin <cetinsamedtarik@gmail.com>
2019-06-13 22:00:57 +0300

    swipe direction detection is working

---
+++ hexfall-clone/Assets/game/code/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using Lean.Touch;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private LeanFingerSwipe _rightSwipeDetector, _leftSwipeDetector;
+
+    private void Start()
+    {
+        _rightSwipeDetector.OnSwipe.AddListener(OnRightSwipe);
+        _leftSwipeDetector.OnSwipe.AddListener(OnLeftSwipe);
+    }
+
+    private void OnRightSwipe(LeanFinger finger)
+    {
+        Debug.Log(nameof(InputManager) + " right swipe");
+    }
+
+    private void OnLeftSwipe(LeanFinger finger)
+    {
+        Debug.Log(nameof(InputManager) + " left swipe");
+    }
+}
