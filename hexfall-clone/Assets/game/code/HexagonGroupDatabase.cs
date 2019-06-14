67fa0ab438da674d61037aa4021dffb192dae72f
starikcetin <cetinsamedtarik@gmail.com>
2019-06-14 17:03:56 +0300

    NuGet for Unity imported from https://github.com/GlitchEnzo/NuGetForUnity/releases

---
+++ hexfall-clone/Assets/game/code/HexagonGroupDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using starikcetin.hexfallClone;
+using UnityEngine;
+
+public class HexagonGroupDatabase : Singleton<HexagonGroupDatabase>
+{
+    private readonly List<HexagonGroup> _hexagonGroups = new List<HexagonGroup>();
+    public IReadOnlyCollection<HexagonGroup> HexagonGroups => _hexagonGroups.AsReadOnly();
+
+    public void RegisterHexagonGroup(HexagonGroup group)
+    {
+        _hexagonGroups.Add(group);
+    }
+//
+//    public void FindClosestGroup(Vector3 point, float size)
+//    {
+//        _hexagonGroups.MinBy(g => Vector3.SqrMagnitude(point - g.GetCenter(size)));
+//    }
+}
