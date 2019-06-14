4a4c29a9a410fb91ba71ed14aee1da6559655469
starikcetin <cetinsamedtarik@gmail.com>
2019-06-14 16:29:50 +0300

    tap detection

--- hexfall-clone/Assets/game/code/InputManager.cs
+++ hexfall-clone/Assets/game/code/InputManager.cs
@@ -1,14 +1,17 @@
-﻿using Lean.Touch;
+﻿using System;
+using Lean.Touch;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private LeanFingerSwipe _rightSwipeDetector, _leftSwipeDetector;
+    [SerializeField] private LeanFingerTap _tapDetector;
 
     private void Start()
     {
         _rightSwipeDetector.OnSwipe.AddListener(OnRightSwipe);
         _leftSwipeDetector.OnSwipe.AddListener(OnLeftSwipe);
+        _tapDetector.OnTap.AddListener(OnTap);
     }
 
     private void OnRightSwipe(LeanFinger finger)
@@ -19,5 +22,15 @@ public class InputManager : MonoBehaviour
     private void OnLeftSwipe(LeanFinger finger)
     {
         Debug.Log(nameof(InputManager) + " left swipe");
+    }
+
+    private void OnTap(LeanFinger finger)
+    {
+        var screenPos = finger.ScreenPosition;
+        var worldPos = finger.GetWorldPosition(10, Camera.current);
+
+        Debug.Log($"{nameof(InputManager)} + tap " +
+                  $"| {nameof(screenPos)} = {screenPos} " +
+                  $"| {nameof(worldPos)} = {worldPos}");
     }
 }
