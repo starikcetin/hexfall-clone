fcdeff237896d7910032e34f6da40c7399d9f083
starikcetin <cetinsamedtarik@gmail.com>
2019-06-16 20:26:51 +0300

    new game button action implemented in TitleScreenUiManager and wired up in editor.

---
+++ hexfall-clone/Assets/TitleSceneUiManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Eflatun.UnityCommon.Utils.UI;
+using UnityEngine;
+
+public class TitleSceneUiManager : MonoBehaviour
+{
+    [SerializeField] private EventButton _newGameButton;
+
+    private void Start()
+    {
+        _newGameButton.OnClick += NewGameButtonOnClick;
+    }
+
+    private void OnDestroy()
+    {
+        if (_newGameButton)
+        {
+            _newGameButton.OnClick -= NewGameButtonOnClick;
+        }
+    }
+
+    private void NewGameButtonOnClick()
+    {
+        Debug.Log(nameof(NewGameButtonOnClick));
+
+        LoadGameScene();
+        InitializeNewGame();
+        UnloadTitleScene();
+    }
+
+    private void LoadGameScene()
+    {
+        Debug.Log(nameof(LoadGameScene));
+
+        throw new NotImplementedException();
+    }
+
+    private void InitializeNewGame()
+    {
+        Debug.Log(nameof(InitializeNewGame));
+
+        throw new NotImplementedException();
+    }
+
+    private void UnloadTitleScene()
+    {
+        Debug.Log(nameof(UnloadTitleScene));
+
+        throw new NotImplementedException();
+    }
+}
