f805ced790aba9592bde28a86f58999c1831d93b
starikcetin <cetinsamedtarik@gmail.com>
2019-06-18 04:13:15 +0300

    exit button handling and multitouch disable

---
+++ hexfall-clone/Assets/game/code/InputParameterSetter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class InputParameterSetter : MonoBehaviour
+{
+    private void Start()
+    {
+        Input.backButtonLeavesApp = true;
+        Input.multiTouchEnabled = false;
+    }
+}
